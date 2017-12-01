3617c6c88ccbc9b9b0a3ecda7301448f0abc8faf
Ícaro Harry <me@icaro.io>
2017-12-01 19:02:49 -0200

    Include request-promise

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const steem = require('steem');
-const request = require('request');
+const request = require('request-promise');
 
-request({
-  method: 'GET',
-  uri: 'https://api.utopian.io/api/posts/?limit=1&skip=0&section=all&sortBy=created&filterBy=any&type=all'
-}, (error, response, body) => {
-  console.log(JSON.parse(body));
-});
+const limit = 3;
+const skip = 0;
+const section = ['tutorials', 'development'];
+const sortBy = 'created';
+const filterBy = 'any';
+const type = 'all';
 
+function run() {
+  setInterval(async () => {
+    const posts = JSON.parse(
+      await request.get(`https://api.utopian.io/api/posts/?limit=${limit}&skip=${skip}&section=${section}&sortBy=${sortBy}&filterBy=${filterBy}&type=${type}`)
+    );
+    console.log(posts);
+  }, 1000);
+}
+
+run()
 // steem.broadcast.vote(process.env.POSTING_KEY, process.env.ACCOUNT, 'guifaquetti', 'who-am-i-quem-sou-eu', 50, function(err, result) {
 //   console.log(err, result);
 // });
